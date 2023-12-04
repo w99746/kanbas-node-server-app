2bd0a77444e6fa69631b820e4c7bc0420da80b28
yunkai wang <w2247282914@gmail.com>
2023-12-03 22:00:09 -0500

    a6 Mongo fa23

--- app.js
+++ app.js
@@ -5,11 +5,37 @@ import Lab5 from './lab5.js';
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import "dotenv/config";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+
+import session from "express-session";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+mongoose.connect(CONNECTION_STRING);
 
 const app = express()
+app.use(cors({
+   credentials: true,
+   origin: process.env.FRONTEND_URL
+}));
 
-app.use(cors());
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }
+  app.use(session(sessionOptions));
+  
+  
 app.use(express.json());
+UserRoutes(app);
 Hello(app)
 ModuleRoutes(app);
 Lab5(app);
